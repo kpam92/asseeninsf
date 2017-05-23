1ac38877e11a4975179bed0624a212415d261252
kpam92 <kpam1992@gmail.com>
2017-05-22 21:45:02 -0700

    initial migration

--- db/migrate/20170523044005_create_locations.rb
+++ db/migrate/20170523044005_create_locations.rb
@@ -1,7 +1,13 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-
+      t.string :title, null: false
+      t.string :actor_1, null: false
+      t.string :actor_2, null: false
+      t.string :director, null: false
+      t.string :fun_facts
+      t.string :locations, null: false
+      t.integer :release_year, null: false
       t.timestamps null: false
     end
   end
