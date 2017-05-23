02e7060297705950fedb5ab15fafbdbea9c52600
kpam92 <kpam1992@gmail.com>
2017-05-22 21:15:49 -0700

    react router root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    // Router goes here...
+    <HashRouter>
+      <App/>
+    </HashRouter>
   </Provider>
 );
