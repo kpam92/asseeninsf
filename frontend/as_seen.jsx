7d40d9f56fa09113fc204f2104e2bbed1d3ae3fd
kpam92 <kpam1992@gmail.com>
2017-05-23 08:39:02 -0700

    both actions work, need context now

--- frontend/as_seen.jsx
+++ frontend/as_seen.jsx
@@ -3,12 +3,12 @@ import configureStore from './store/store';
 import Root from './components/root';
 import React from 'react';
 import { fetchLocations } from './util/location_api_util';
+import { receiveLocations } from './actions/location_actions';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
   window.store = store;
-  window.fetchLocations = fetchLocations;
   window.fetchLocations = fetchLocations;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
--- frontend/util/location_api_util.js
+++ frontend/util/location_api_util.js
@@ -3,7 +3,6 @@ export const fetchLocations = () => {
   return $.ajax({
     method: 'GET' ,
     url: 'https://data.sfgov.org/resource/wwmu-gmzc.json',
-    error: (err) => console.log(err),
-    success: (data) => console.log(data)
+    error: (err) => console.log(err)
   })
 }
