0e9aaa548820a81609a534eb1ae6b638112e37fe
kpam92 <kpam1992@gmail.com>
2017-05-22 23:14:19 -0700

    reducers

--- frontend/as_seen.jsx
+++ frontend/as_seen.jsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import React from 'react';
+import { fetchLocations } from './util/location_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   // const store = configureStore();
+  window.fetchLocations = fetchLocations;
   const root = document.getElementById('root');
   ReactDOM.render(<Root/>, root);
 });
--- frontend/util/location_api_util.js
+++ frontend/util/location_api_util.js
@@ -1,8 +1,9 @@
 
 export const fetchLocations = () => {
   return $.ajax({
-    method: // ,
-    url: //,
-    error: (err) => console.log(err)
+    method: 'GET' ,
+    url: 'https://data.sfgov.org/resource/wwmu-gmzc.json',
+    error: (err) => console.log(err),
+    success: (data) => console.log(data)
   })
 }
