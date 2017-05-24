89d5c8b3b74b346ab9ec85fa3d0efbd9fd3622e0
kpam92 <kpam1992@gmail.com>
2017-05-23 22:21:11 -0700

    marker manager

--- frontend/components/location_map/location_map.jsx
+++ frontend/components/location_map/location_map.jsx
@@ -1,40 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { withRouter } from 'react-router-dom';
-//
-// // import MarkerManager from '../../util/marker_manager';
-//
-// class LocationMap extends React.Component {
-//   //...
-//
-//   componentDidMount() {
-//     // set the map to show SF
-//     const mapOptions = {
-//       center: { lat: 37.7758, lng: -122.435 }, // this is SF
-//       zoom: 13
-//     };
-//
-//     // wrap the mapDOMNode in a Google Map
-//     this.map = new google.maps.Map(this.mapNode, mapOptions);
-//   }
-//
-//   render() {
-//     return (
-//       <div className="map" ref="map">
-//         Map
-//       </div>
-//     );
-//   }
-//
-//   //...
-// }
-//
-// export default LocationMap;
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
-
+import MarkerManager from '../../util/marker_manager';
 
 const getCoordsObj = latLng => ({
   lat: latLng.lat(),
@@ -53,9 +20,13 @@ class LocationMap extends React.Component {
   componentDidMount() {
     const map = this.refs.map;
     this.map = new google.maps.Map(map, mapOptions);
+    this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager.updateMarkers();
     }
 
-
+    componentDidUpdate() {
+      this.MarkerManager.updateMarkers();
+    }
   render(){
     return (
       <div className="map" ref="map">
