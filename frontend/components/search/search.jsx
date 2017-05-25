c340a85489390a7ab6c0420ae029a7cc5e4f2b85
kpam92 <kpam1992@gmail.com>
2017-05-25 13:05:57 -0700

    props to location_map

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import LocationIndex from './location_index';
 import LocationMap from '../location_map/location_map';
 
-const Search = ({ locations, fetchLocations }) => (
+const Search = ({ locations, fetchLocations, updateBounds }) => (
   <div className="user-pane">
     <div className="left-half">
       <LocationMap locations={locations}/>
-      <LocationIndex locations={locations} fetchLocations={fetchLocations}/>
+      <LocationIndex locations={locations} fetchLocations={fetchLocations} updateBounds={updateBounds}/>
     </div>
   </div>
 );
