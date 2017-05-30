70683dc2bd9c3e62a984450837349790aa71624a
kpam92 <kpam1992@gmail.com>
2017-05-30 15:18:49 -0700

    creating action for fetching single movie info

--- frontend/util/location_api_util.js
+++ frontend/util/location_api_util.js
@@ -23,3 +23,12 @@ export const fetchLocations = (data) => {
     data
   })
 }
+
+export const fetchSingleLocationInfo = (data) => {
+  return $.ajax({
+    method: 'GET' ,
+    url: `https://api.themoviedb.org/3/search/movie?api_key=00733eaa74c3c5021be9a3f9993f5a4b&language=en-US&query=Godzilla&page=1&include_adult=false&year=2014`,
+    success: (err) => console.log(err),
+
+  })
+}
