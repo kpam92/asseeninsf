1d2a9627a74a5a2076e5cf4cb3fbf21e85d65ee2
kpam92 <kpam1992@gmail.com>
2017-06-01 23:44:16 -0700

    fetches information from movie api, then displays in location detail

--- frontend/components/search/location_detail.jsx
+++ frontend/components/search/location_detail.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const LocationDetail = ({ movie }) => (
   <div className="user-pane">
-
+    <h1>hi</h1>
+    <h1>{movie.overview}</h1>
+    <img src={"https://image.tmdb.org/t/p/w1280" + movie.poster_path}/>
   </div>
 );
 
--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -5,12 +5,13 @@ import FilterForm from './filter_form';
 import LocationDetail from './location_detail';
 import LocationMap from '../location_map/location_map';
 
-const Search = ({ locations, fetchLocations, updateBounds, updateFilter }) => (
+const Search = ({ locations, fetchLocations, updateBounds, updateFilter, movie }) => (
   <div className="user-pane">
     <div className="left-half">
       <LocationMap locations={locations}  updateBounds={updateBounds} updateFilter={updateFilter}/>
       <LocationIndex locations={locations}/>
       <FilterForm updateFilter={updateFilter}/>
+      <LocationDetail movie={movie}/>
     </div>
   </div>
 );
