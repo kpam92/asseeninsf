f27b4e7d4de92cb332a1dc1d78b6412b90c1c88d
kpam92 <kpam1992@gmail.com>
2017-06-01 23:06:34 -0700

    reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import LocationsReducer from './locations_reducer';
 import FilterReducer from './filter_reducer';
+import LocationDetailReducer from './location_detail_reducer';
 
 const rootReducer = combineReducers({
   locations: LocationsReducer,
