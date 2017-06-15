1817122b0133059803d327b0a87a80a5a2ffb8cd
kpam92 <kpam1992@gmail.com>
2017-06-15 15:26:28 -0700

    styling map

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -1,3 +1,13 @@
+
+var symbolIcon = {
+  path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+  fillColor: '#ffed00',
+  fillOpacity: 0.8,
+  scale: .5,
+  // strokeColor: 'gold',
+  // strokeWeight: 4
+};
+
 export default class MarkerManager {
 
   constructor(map, handleClick) {
@@ -25,7 +35,8 @@ export default class MarkerManager {
     const marker = new google.maps.Marker({
       position,
       map: this.map,
-      locationId: location.id
+      locationId: location.id,
+      icon: symbolIcon
     });
 
     marker.addListener('click', () => this.handleClick(location,marker));
